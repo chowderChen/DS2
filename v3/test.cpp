afcfd5b17963e4c3061c4f045711d77036c4974b
chowderChen <55129325+chowderChen@users.noreply.github.com>
2022-05-14 22:01:35 +0800

    Add files via upload

---
+++ v3/test.cpp
@@ -0,0 +1,12 @@
+#include "hash_v2.h"
+using namespace std ;
+
+int main() {
+    int num = 50 ;
+    cout << num << endl ;
+    for ( int i = 0 ; i < 6 ; i++ ) {
+        num = num * 50 ;
+        cout << num << endl ;
+    }
+    return 0 ;
+}
